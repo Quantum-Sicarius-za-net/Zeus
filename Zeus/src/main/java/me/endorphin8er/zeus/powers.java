b15011bc3a6afda846be754ce1cc2247a3ec0c81
Quantum-Sicarius <thomas@visco.co.za>
2012-05-04 20:21:00 +0200

    Added Licences, fixed syntax

--- Zeus/src/main/java/me/endorphin8er/zeus/powers.java
+++ Zeus/src/main/java/me/endorphin8er/zeus/powers.java
@@ -1,3 +1,20 @@
+/*
+This file is part of Zeus.
+
+Zeus is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Zeus is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Zeus.  If not, see http://www.gnu.org/licenses/.
+*/
+
 package me.endorphin8er.zeus;
 
 import org.bukkit.Location;
@@ -15,14 +32,12 @@ public class powers implements Listener {
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		int blockId = player.getItemInHand().getType().getId();
-				if (blockId == 369){
-					Block block = player.getTargetBlock(null, 50);
-					Location location = block.getLocation();
-					World world = player.getWorld();
-					world.strikeLightning(location);
-					
+		if (blockId == 369){
+			Block block = player.getTargetBlock(null, 50);
+			Location location = block.getLocation();
+			World world = player.getWorld();
+			world.strikeLightning(location);		
 
 		}
 	}
-
 }
--- Zeus/src/main/java/me/endorphin8er/zeus/zeus.java
+++ Zeus/src/main/java/me/endorphin8er/zeus/zeus.java
@@ -1,3 +1,20 @@
+/*
+This file is part of Zeus.
+
+Zeus is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Zeus is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Zeus.  If not, see http://www.gnu.org/licenses/.
+*/
+
 package me.endorphin8er.zeus;
 
 import java.util.logging.Logger;
@@ -17,26 +34,32 @@ public class zeus extends JavaPlugin{
 	@Override
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Is Now Disabled!");}
+		this.logger.info(pdfFile.getName() + " Is Now Disabled!");
+	}
 	
 
 		
-		@Override
-		public void onEnable() {
-			PluginDescriptionFile pdfFile = this.getDescription();
-			this.logger.info(pdfFile.getName() + " Is Now Enabled!");}
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Is Now Enabled!");
+	}
 
 			
-		public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-			Player player = (Player) sender;
-			if(commandLabel.equalsIgnoreCase("zeus")){
-				if(args.length == 0){
-					if(player.isOp());
-			player.sendMessage("----" + ChatColor.GREEN + "Zeus Help" + ChatColor.WHITE + "----");
-			player.sendMessage(ChatColor.GOLD + "Use A Blazrod For The Power Of Thor!");
-			player.sendMessage(ChatColor.GRAY + "Use Gunpowder For The Power Of The Creeper Lord!");
-				}else{
-					player.sendMessage(ChatColor.DARK_RED + "You Are Not A God");
-
-}}
-			return false;}}
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		Player player = (Player) sender;
+		if(commandLabel.equalsIgnoreCase("zeus")) {
+			if(args.length == 0) {
+				if(player.isOp()) {
+					player.sendMessage("----" + ChatColor.GREEN + "Zeus Help" + ChatColor.WHITE + "----");
+					player.sendMessage(ChatColor.GOLD + "Use A Blazrod For The Power Of Thor!");
+					player.sendMessage(ChatColor.GRAY + "Use Gunpowder For The Power Of The Creeper Lord!");
+				}
+			}
+			else {
+				player.sendMessage(ChatColor.DARK_RED + "You Are Not A God");
+			}
+		}
+		return false;
+	}
+}
